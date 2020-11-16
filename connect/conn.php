9d0044f04696fe6a4af6d05375cdec47fbbeaf96
fcotabar <fcotabar@gmail.com>
2020-11-16 13:35:17 -0500

    Initialization

--- connect/conn.php
+++ connect/conn.php
@@ -9,11 +9,21 @@ $dbport = "8889";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database, $dbport);
 
-// Check connection
+
+//Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die('Connect Error (' . $conn->connect_errno . ') '
+          . $conn->connect_error);
 }
-echo "Connected successfully";
+
+//Set Charset to UTF8
+if (!$conn->set_charset("utf8")) {
+  printf("Error loading character set utf8: %s\n", $conn->error);
+  exit();
+}
+
+
+
 ?>
 
 
---
+++ inserts/insert-countries.php
@@ -0,0 +1,8 @@
+<?php
+$functionsPath = $_SERVER['DOCUMENT_ROOT']."/connect/";
+include_once($functionsPath . "con.php");
+
+
+
+
+?>
